b36c8cf9ea9af881d4e7be7291fe64fbe5a3c126
LalitKishor <lalitsinghbanna05@gmail.com>
2015-11-06 10:02:18 +0530

    cat clicker project

---
+++ cat_clickerjs.js
@@ -0,0 +1,11 @@
+ 
+	 var CLICK=1;
+	 if (CLICK!=0){
+				$("#image").click( function(){
+				alert("the number of click "+ CLICK + " clicks");
+				CLICK=CLICK+1;
+			});
+				
+			 
+            };
+	
